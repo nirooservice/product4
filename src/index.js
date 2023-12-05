6099f432e4fa954ba56b23793f5485b5e9899ba9
Nirroservice <jhon.smith6384@gmail.com>
2023-12-05 21:26:15 +0100

    Fix syntax error in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';  // Import BrowserRouter
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,8 +9,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router basename="/product4">
+      <App />
+    </Router>
+  </React.StrictMode>,  // Add a comma here
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
